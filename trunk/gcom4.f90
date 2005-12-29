8c1ac532f7937b2bf3b90857330c0195d3012337
k1jt <k1jt@ab8295b8-cf94-4d9e-aec4-7959e3be5d79>
2005-12-29 13:55:28 +0000

    Removed the undesired WSJT directory from .../wsjt/WSJT/trunk

---
+++ trunk/gcom4.f90
@@ -0,0 +1,10 @@
+! Variable             Purpose                              Set in Thread
+!-------------------------------------------------------------------------
+character addpfx*4     !Add-on prefix, as in ZA/PA2CHR           GUI
+integer*2 d2c          !Rx data recovered from recorded file     GUI
+integer jzc            !Length of data available in d2c          GUI
+character filename*24  !Name of wave file read from disk         GUI
+
+common/gcom4/addpfx,d2c(661500),jzc,filename
+
+!### volatile /gcom4/
